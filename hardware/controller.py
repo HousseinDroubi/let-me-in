61420b8538dbd0a3e5c31bafa0348cd3dd693044
HousseinDroubi <housseinalialdroubi@gmail.com>
2022-11-06 16:08:57 +0000

    Hardware-controller.py: implement a function to initiate the stepper motor

--- hardware/controller.py
+++ hardware/controller.py
@@ -76,3 +76,12 @@ def turnOffLeds():
     turnOffRed()
     turnOffGreen()
     turnOffYellow()
+
+#The below function will initiate the stepper motor at pin 11,
+#12 ,13 and 15. These pins are plugged into a motor controller
+#called L298N Motor Driver Module
+def initiate_Barrier():
+    GPIO.setup(11,GPIO.OUT)
+    GPIO.setup(12,GPIO.OUT)
+    GPIO.setup(13,GPIO.OUT)
+    GPIO.setup(15,GPIO.OUT)
