f23210aa18ce3016ab6f8e9843464cba2e374f34
HousseinDroubi <housseinalialdroubi@gmail.com>
2022-11-06 14:54:23 +0000

    Hardware-controller.py: set GPIO mode as BOARD

--- hardware/controller.py
+++ hardware/controller.py
@@ -6,3 +6,6 @@ from dotenv import load_dotenv
 from picamera import PiCamera
 from PIL import Image
 load_dotenv()
+
+# GPIO.BOARD mode will refer to the number of the pin in the plug
+GPIO.setmode(GPIO.BOARD)
