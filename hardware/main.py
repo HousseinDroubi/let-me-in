58d9d5b5be0495f430f1a6a555c2b51c38ca76a0
HousseinDroubi <housseinalialdroubi@gmail.com>
2022-11-06 18:55:17 +0000

    Hardware-main.py: initiate components

--- hardware/main.py
+++ hardware/main.py
@@ -1,2 +1,9 @@
-import functions as fct
+import controller as fct
 import time
+
+#Initiate stepper motor
+fct.initiate_Barrier()
+#Initiate IRSensor
+fct.initiate_IRSensor()
+#Initiate Leds
+fct.initiate_Leds()
