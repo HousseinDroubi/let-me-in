c9b011f538095ec15ad9059aee8abbaf28c6b5e6
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-07 22:48:20 +0200

    Backend-admin middleware:implement functionality

--- backend/app/Http/Middleware/AdminMiddleware.php
+++ backend/app/Http/Middleware/AdminMiddleware.php
@@ -14,8 +14,10 @@ class AdminMiddleware
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle(Request $request, Closure $next)
-    {
+    public function handle(Request $request, Closure $next){
+          
+        if(auth('api')->user()->id!='1')
+            return redirect()->route('access-denied');
         return $next($request);
     }
 }
