74758b9f5675b17158acd4aee76e20a6d111ae0b
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-07 19:49:38 +0200

    Backend-user controller-add user: implement functionality

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -14,6 +14,25 @@ use App\Http\Controllers\AuthController;
 class UserController extends Controller{
 
     public function addUser(Request $request){
-        
+        $validator = Validator::make($request->all(), [
+            'username' => 'required|string|min:3|max:20',
+            'car_type' => 'required|string|min:3|max:30',
+            'car_plate_number' => 'required|string|min:2|max:7|unique:user_details',
+            'profile_url' => 'string',
+        ]);
+        if($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+        if($request->profile_url){
+            try {
+                $request->profile_url =  (new AuthController)->saveImage($request->profile_url);
+                
+            } catch (\Throwable $th) {
+                return response()->json([
+                    'message' => 'Error while adding a user',
+                    'err'=> $th->getMessage()
+                ], 400);
+            }
+        }
     }
 }
