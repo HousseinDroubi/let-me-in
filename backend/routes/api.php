228f3281201de0dbf72e2995cae270b9756f3b8a
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-07 22:50:35 +0200

    Backend-api : implement admin middleware

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -9,17 +9,20 @@ use App\Http\Controllers\EventController;
 
 Route::group(["prefix"=> "v0.1"], function(){
     Route::group(["middleware" => "auth:api"], function(){
-        Route::get("/get_admin_data", [AuthController::class, "getAdminData"])->name("get-admin-data");
-        Route::post("/update_admin_data", [AuthController::class, "updateAdminData"])->name("update-admin-data");
-        Route::post("/change_admin_password", [AuthController::class, "changeAdminPassowrd"])->name("change-admin-password");
-        Route::post("/add_user", [UserController::class, "addUser"])->name("add-user");
-        Route::post("/edit_user", [UserController::class, "updateUserData"])->name("edit-user");
-        Route::post("/block_user", [UserController::class, "blockUser"])->name("block-user");
-        Route::post("/unblock_user", [UserController::class, "unblockUser"])->name("unblock-user");
-        Route::get("/users", [UserController::class, "getUsers"])->name("users");
-        Route::get("/blocked_users", [UserController::class, "getBlockedUsers"])->name("blocked-users");
-        Route::get("/get_events", [EventController::class, "getEvents"])->name("get-events");
-        Route::post("/change_barrier_status", [BarrierController::class, "changeBarrierStatus"])->name("change-barrier-status");
+        
+        Route::group(["middleware" => "adminmiddleware"], function(){
+            Route::get("/get_admin_data", [AuthController::class, "getAdminData"])->name("get-admin-data");
+            Route::post("/update_admin_data", [AuthController::class, "updateAdminData"])->name("update-admin-data");
+            Route::post("/change_admin_password", [AuthController::class, "changeAdminPassowrd"])->name("change-admin-password");
+            Route::post("/add_user", [UserController::class, "addUser"])->name("add-user");
+            Route::post("/edit_user", [UserController::class, "updateUserData"])->name("edit-user");
+            Route::post("/block_user", [UserController::class, "blockUser"])->name("block-user");
+            Route::post("/unblock_user", [UserController::class, "unblockUser"])->name("unblock-user");
+            Route::get("/users", [UserController::class, "getUsers"])->name("users");
+            Route::get("/blocked_users", [UserController::class, "getBlockedUsers"])->name("blocked-users");
+            Route::get("/get_events", [EventController::class, "getEvents"])->name("get-events");
+            Route::post("/change_barrier_status", [BarrierController::class, "changeBarrierStatus"])->name("change-barrier-status");
+        });
 
         Route::post("/add_update_event", [EventController::class, "addOrUpdateEvent"])->name("add-update-event");
         Route::post("/check_car_plate", [EventController::class, "checkCarPlate"])->name("check-car-plate");
