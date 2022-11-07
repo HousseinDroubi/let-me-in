75008968e9959dcca3281141b85bf13384bc8964
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-07 14:47:01 +0200

    Backend-barrier status model:implement contents

--- backend/app/Models/BarrierStatus.php
+++ backend/app/Models/BarrierStatus.php
@@ -5,7 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class BarrierStatus extends Model
-{
+class BarrierStatus extends Model{
+
     use HasFactory;
+
+    protected $fillable = [
+        'status',
+    ];
 }
