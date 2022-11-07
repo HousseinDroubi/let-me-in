c615f95fde6a43e13cf366c9d9b400131d205d37
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-07 15:08:40 +0200

    Backend-user model:add comments

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -15,17 +15,24 @@ class User extends Model{
         'user_type',
     ];
 
+    // The rows in table user of user_type = '1', has only one record into admin_details table, which is for the amdin
     public function adminDetail(){
         return $this->hasOne(AdminDetail::class);
     }
+
+    // The rows in table user of user_type = '0', has only one record into user_details table, which is the user
     public function userDetail(){
         return $this->hasOne(UserDetail::class);
     }
+
+    // The rows in table user of user_type = '0', may have many records into events table
     public function event(){
         return $this->hasMany(Event::class);
     }
+
+    // The rows in table user of user_type = '1', may have many records into admin_details table, which is the admin
     public function code(){
         return $this->hasMany(Code::class);
     }
-    
+
 }
