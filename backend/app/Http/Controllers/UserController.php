3ba5f4d033df84d8269321ad537bda21219c310f
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-07 20:02:29 +0200

    Backend-user controller-update user: edit left margin

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -59,62 +59,62 @@ class UserController extends Controller{
             ], 201);
         }
 
-        public function updateUserData(Request $request){
-            $validator = Validator::make($request->all(), [
-                'id' => 'required|integer|min:3',
-                'username' => 'string|min:3|max:20|unique:users',
-                'car_type' => 'string|min:3|max:30',
-                'car_plate_number' => 'string|min:2|max:7|unique:user_details',
-                'profile_url' => 'string',
-                'decision'=>'integer|min:0|max:1'
-            ]);
-            if($validator->fails()) {
-                return response()->json($validator->errors(), 400);
-            }
-            // Since the id is required. Hence, if the count of the fields of request is 1, that means nothing
-            // has been sent with the id
-            else if(count($request->all()) == 1){
-                return response()->json([
-                    'message' => 'At least one field required'
-                ], 400);
-            }
-            // Get the user in order to update data
-            $user = User::find($request->id);
-            if(!$user){
-                return response()->json([
-                    'message' => 'User not found'
-                ], 400);
-            }
-
-            // The decision here is for the car plate that's neither in the approved list, nor in the blocked list.
-            // Hence, in case this user has been approved or blocked from admin, the decision must be '0' or '1'
-            // Otherwise, the admin will be updating a user from users page
-            if($request->decision)
-            $user->status = $request->decision;
-        
-            $user->username = ($request->username!='') ? $request->username : $user->username;
-            $user->userDetail->car_type = ($request->car_type!='') ? $request->car_type : $user->userDetail->car_type;
-            $user->userDetail->car_plate_number = ($request->car_plate_number!='') ? $request->car_plate_number : $user->userDetail->car_plate_number;
-
-            if($request->profile_url){
-                try {
-                    unlink($user->profile_url);
-                    $user->profile_url = (new AuthController)->saveImage($request->profile_url);
-                } catch (\Throwable $th) {
-                    return response()->json([
-                        'message' => 'Error while updating user',
-                        'err'=> $th->getMessage()
-                    ], 400);
-                }
-            }
-            if($user->save() && $user->userDetail->save()){
-                return response()->json([
-                    "message" => "user updated Successfully",
-                    "data" => $user
-                ]);
-            }
+    public function updateUserData(Request $request){
+        $validator = Validator::make($request->all(), [
+            'id' => 'required|integer|min:3',
+            'username' => 'string|min:3|max:20|unique:users',
+            'car_type' => 'string|min:3|max:30',
+            'car_plate_number' => 'string|min:2|max:7|unique:user_details',
+            'profile_url' => 'string',
+            'decision'=>'integer|min:0|max:1'
+        ]);
+        if($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+        // Since the id is required. Hence, if the count of the fields of request is 1, that means nothing
+        // has been sent with the id
+        else if(count($request->all()) == 1){
             return response()->json([
-                "message" => "Error while updating user"
+                'message' => 'At least one field required'
+            ], 400);
+        }
+        // Get the user in order to update data
+        $user = User::find($request->id);
+        if(!$user){
+            return response()->json([
+                'message' => 'User not found'
+            ], 400);
+        }
+
+        // The decision here is for the car plate that's neither in the approved list, nor in the blocked list.
+        // Hence, in case this user has been approved or blocked from admin, the decision must be '0' or '1'
+        // Otherwise, the admin will be updating a user from users page
+        if($request->decision)
+        $user->status = $request->decision;
+    
+        $user->username = ($request->username!='') ? $request->username : $user->username;
+        $user->userDetail->car_type = ($request->car_type!='') ? $request->car_type : $user->userDetail->car_type;
+        $user->userDetail->car_plate_number = ($request->car_plate_number!='') ? $request->car_plate_number : $user->userDetail->car_plate_number;
+
+        if($request->profile_url){
+            try {
+                unlink($user->profile_url);
+                $user->profile_url = (new AuthController)->saveImage($request->profile_url);
+            } catch (\Throwable $th) {
+                return response()->json([
+                    'message' => 'Error while updating user',
+                    'err'=> $th->getMessage()
+                ], 400);
+            }
+        }
+        if($user->save() && $user->userDetail->save()){
+            return response()->json([
+                "message" => "user updated Successfully",
+                "data" => $user
             ]);
         }
+        return response()->json([
+            "message" => "Error while updating user"
+        ]);
+    }
 }
