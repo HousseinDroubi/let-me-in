28d0578b1da5404496c868e4051dd4f13de379d4
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-07 14:45:51 +0200

    Backend: create barrier status model

---
+++ backend/app/Models/BarrierStatus.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BarrierStatus extends Model
+{
+    use HasFactory;
+}
