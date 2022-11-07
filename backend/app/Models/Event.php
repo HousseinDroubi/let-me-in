1d18f9d5741f7ba31c221f58af19303566d16789
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-07 14:55:17 +0200

    Backend-event model:implement contents

--- backend/app/Models/Event.php
+++ backend/app/Models/Event.php
@@ -5,7 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Event extends Model
-{
+class Event extends Model{
+    
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'arrival_time',
+        'departure_time',
+    ];
 }
