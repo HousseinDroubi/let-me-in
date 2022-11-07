0287bd19aa0fade9685704d14b1cf92b82e8e76e
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-07 19:01:42 +0200

    Backend-api : implement change forgotten password route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -13,5 +13,6 @@ Route::group(["prefix"=> "v0.1"], function(){
     Route::get("/access_denied", [AuthController::class, "denyAccess"])->name("access-denied");
     Route::post("/send_code", [AuthController::class, "sendCode"])->name("send-code");
     Route::post("/verify_code", [AuthController::class, "verifyCode"])->name("verify-code");
+    Route::post("/change_forgotten_password", [AuthController::class, "changeForgottenPassword"])->name("change-forgotten-password");
 
 });
