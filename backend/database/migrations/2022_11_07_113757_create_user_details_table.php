3ede00b79c684046a8100d159dd87dc5d25b15f7
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-07 13:39:40 +0200

    Backend-database: implement user details table

--- backend/database/migrations/2022_11_07_113757_create_user_details_table.php
+++ backend/database/migrations/2022_11_07_113757_create_user_details_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('user_details', function (Blueprint $table) {
             $table->id();
+            $table->string('user_id')->unique();
+            $table->string('car_type')->default('Unkown');
+            $table->string('car_plate_number')->unique();
+            $table->integer('status')->default('0');
             $table->timestamps();
         });
     }
