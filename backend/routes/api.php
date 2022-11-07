5e79ee779960c94eebe2da387396b2a2155fa987
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-07 17:58:05 +0200

    Backend-api : implement login route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,5 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::group(["prefix"=> "v0.1"], function(){
+    Route::post("/login", [AuthController::class, "login"])->name("login");
     Route::post("/register", [AuthController::class, "register"])->name("register");
 });
