cd22296092186309d7ac07619e70b8d251c0246e
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-07 16:08:04 +0200

    Backend-acknowledgement mail: clean code

--- backend/app/Mail/Acknowledgement.php
+++ backend/app/Mail/Acknowledgement.php
@@ -14,54 +14,35 @@ class Acknowledgement extends Mailable
     use Queueable, SerializesModels;
 
     public $username,$arrival_time,$car_plate_number;
-    /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
-    public function __construct($username,$arrival_time,$car_plate_number)
-    {
+    
+    public function __construct($username,$arrival_time,$car_plate_number){
+
         $this->username = $username;
         $this->arrival_time = $arrival_time;
         $this->car_plate_number = $car_plate_number;
     }
 
-    /**
-     * Get the message envelope.
-     *
-     * @return \Illuminate\Mail\Mailables\Envelope
-     */
-    public function envelope()
-    {
+    public function envelope(){
+
         return new Envelope(
             subject: 'Acknowledgement',
         );
     }
 
-    /**
-     * Get the message content definition.
-     *
-     * @return \Illuminate\Mail\Mailables\Content
-     */
-    public function content()
-    {
+    public function content(){
+
         return new Content(
             markdown: 'emails.acknowledgements',
         );
     }
 
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array
-     */
-    public function attachments()
-    {
+    public function attachments(){
+
         return [];
     }
 
-    public function build()
-    {
+    public function build(){
+
         return $this->subject('Acknowledgement')
         ->markdown('emails.acknowledgements');
     }
