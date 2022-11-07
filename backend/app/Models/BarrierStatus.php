ef2c5b61bcbc66836d4dcbb82a57463054515f45
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-07 14:48:27 +0200

    Backend-barrier status model:add comments

--- backend/app/Models/BarrierStatus.php
+++ backend/app/Models/BarrierStatus.php
@@ -9,6 +9,8 @@ class BarrierStatus extends Model{
 
     use HasFactory;
 
+    // In the table barrier_statuses, we have one record, and we must return 'status'
+    // of that record
     protected $fillable = [
         'status',
     ];
