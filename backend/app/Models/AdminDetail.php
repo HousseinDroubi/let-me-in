6fe4cdcb20f698f35561af538aed161506dae0fc
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-07 14:37:02 +0200

    Backend-admin detail model:import libraries

--- backend/app/Models/AdminDetail.php
+++ backend/app/Models/AdminDetail.php
@@ -4,8 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class AdminDetail extends Model
+class AdminDetail extends Authenticatable implements JWTSubject
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
 }
