b7f56042b3683df5431cf656cbcf5db53203063b
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-07 16:28:07 +0200

    Backend-auth controller: import libraries

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -2,7 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
+use Validator;
+use App\Models\User;
+use App\Models\Code;
+use App\Models\AdminDetail;
 use Illuminate\Http\Request;
+use App\Mail\Code as MailCode;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 
 class AuthController extends Controller
 {
