eaa5ac89d8c6f3af91d6f35f1cc601709bc47c6c
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-07 13:55:33 +0200

    Backend-barrier statuses table: add comments

--- backend/database/migrations/2022_11_07_114736_create_barrier_statuses_table.php
+++ backend/database/migrations/2022_11_07_114736_create_barrier_statuses_table.php
@@ -7,16 +7,20 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration{
 
     public function up(){
-
+        
+        // In this table, we must have only one record, which declare the barrier status
         Schema::create('barrier_statuses', function (Blueprint $table) {
             $table->id();
+            // The default of status is '2', which means the system of Raspberry Pi must
+            // work normally. While, when the status is '1' or '0', that means the barrier
+            // is still respectively opened or closed until further notice from the admin 
             $table->integer('status')->default('2');
             $table->timestamps();
         });
     }
 
     public function down(){
-        
+
         Schema::dropIfExists('barrier_statuses');
     }
 };
