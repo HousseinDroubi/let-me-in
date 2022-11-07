54ad774dae05f5991f50cb99b9ae0df31d7a1e92
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-07 21:38:53 +0200

    Backend-barrier controller: import libraries

--- backend/app/Http/Controllers/BarrierController.php
+++ backend/app/Http/Controllers/BarrierController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use Validator;
 use Illuminate\Http\Request;
+use App\Models\BarrierStatus;
 
-class BarrierController extends Controller
-{
+
+class BarrierController extends Controller{
+
     //
 }
