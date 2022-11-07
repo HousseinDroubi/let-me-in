dcbfb6c2b977d21df109c9cc6ae28012a8242e4d
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-07 13:49:39 +0200

    Backend-barrier statuses table: clean code

--- backend/database/migrations/2022_11_07_114736_create_barrier_statuses_table.php
+++ backend/database/migrations/2022_11_07_114736_create_barrier_statuses_table.php
@@ -4,15 +4,10 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
+return new class extends Migration{
+
+    public function up(){
+
         Schema::create('barrier_statuses', function (Blueprint $table) {
             $table->id();
             $table->integer('status')->default('2');
@@ -20,13 +15,8 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
+    public function down(){
+        
         Schema::dropIfExists('barrier_statuses');
     }
 };
