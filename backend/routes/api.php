517112d82f0f715f2e280eee924d22792d07d6bb
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-07 20:51:33 +0200

    Backend-api : implement edit user route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,6 +8,7 @@ Route::group(["prefix"=> "v0.1"], function(){
     Route::group(["middleware" => "auth:api"], function(){
         Route::get("/get_admin_data", [AuthController::class, "getAdminData"])->name("get-admin-data");
         Route::post("/add_user", [UserController::class, "addUser"])->name("add-user");
+        Route::post("/edit_user", [UserController::class, "updateUserData"])->name("edit-user");
 
     });
     Route::post("/login", [AuthController::class, "login"])->name("login");
