a5238544a2b01489e4b7200f2087324a22da90e6
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-07 15:15:39 +0200

    Backend-user detail model:implement contents

--- backend/app/Models/UserDetail.php
+++ backend/app/Models/UserDetail.php
@@ -5,7 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class UserDetail extends Model
-{
+class UserDetail extends Model{
+    
     use HasFactory;
+    
+    protected $fillable = [
+        'user_id',
+        'car_type',
+        'car_plate_number',
+        'status',
+    ];
+
 }
