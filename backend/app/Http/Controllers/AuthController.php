78b296156eb14076f0ccaffb75a3b7c154aaf0f8
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-07 16:30:19 +0200

    Backend-auth controller-register: implement validations

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -15,6 +15,14 @@ use Illuminate\Support\Facades\Mail;
 class AuthController extends Controller{
 
     public function register(Request $request){
-        
+        $validator = Validator::make($request->all(), [
+            'username' => 'required|string|min:3|max:20|unique:users',
+            'email' => 'required|string|email|min:5|max:30|unique:admin_details',
+            'password' => 'required|string|min:5|max:30',
+            'profile_url' => 'required|string',
+        ]);
+        if($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
     }
 }
