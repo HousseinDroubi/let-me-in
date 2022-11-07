bca76067c62f7c534e81c47e87cfc784028faab7
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-07 22:07:52 +0200

    Backend-event controller-get events: implement functionality

--- backend/app/Http/Controllers/EventController.php
+++ backend/app/Http/Controllers/EventController.php
@@ -17,7 +17,24 @@ use Illuminate\Support\Facades\Http;
 
 
 class EventController extends Controller{
+
     public function getEvents(){
-        
+        $dates = Event::selectRaw('date(arrival_time) as date')->groupBy('date')->get();
+        $all_events =[];
+        foreach($dates as $date){
+            $day_numbers = json_decode($date);
+            $events_per_day= Event::whereDate('arrival_time','=',$day_numbers->date)->get();
+            foreach($events_per_day as $event_per_day){
+                $event_per_day->user;
+            }
+            $all_events[]=[
+                'date'=>$day_numbers->date,
+                'events'=>$events_per_day,
+            ];
+            }   
+        return response()->json([
+            "status" => "done",
+            "data" => $all_events
+        ]);
     }
 }
