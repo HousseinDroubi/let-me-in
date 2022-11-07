578d13ca0f74ac104d5b4a27095632de1760f69e
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-07 13:40:40 +0200

    Backend-user details table: clean code

--- backend/database/migrations/2022_11_07_113757_create_user_details_table.php
+++ backend/database/migrations/2022_11_07_113757_create_user_details_table.php
@@ -4,15 +4,10 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
+return new class extends Migration{
+
+    public function up(){
+
         Schema::create('user_details', function (Blueprint $table) {
             $table->id();
             $table->string('user_id')->unique();
@@ -23,13 +18,8 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
+    public function down(){
+        
         Schema::dropIfExists('user_details');
     }
 };
