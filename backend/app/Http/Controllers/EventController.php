fa31ea724ffe99569f966ce26b8a2416751a91d6
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-07 22:06:43 +0200

    Backend-event controller: import libraries

--- backend/app/Http/Controllers/EventController.php
+++ backend/app/Http/Controllers/EventController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
+use Validator;
+use App\Models\Admin;
+use App\Models\User;
+use App\Models\Event;
+use App\Models\UserDetail;
+use App\Models\AdminDetail;
+use App\Mail\Acknowledgement;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Http;
 
-class EventController extends Controller
-{
-    //
+
+class EventController extends Controller{
+    
 }
