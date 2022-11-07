cbf5d6ed31110d5a2d35c0b24b30f7e93ec95c64
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-07 22:02:19 +0200

    Backend-api : import BarrierController

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\BarrierController;
 Route::group(["prefix"=> "v0.1"], function(){
     Route::group(["middleware" => "auth:api"], function(){
         Route::get("/get_admin_data", [AuthController::class, "getAdminData"])->name("get-admin-data");
