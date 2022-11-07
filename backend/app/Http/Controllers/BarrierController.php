4ce9bd3ffdd8ca63afa44560ee1ed90c3839b4f7
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-07 21:45:28 +0200

    Backend-barrier controller-get barrier status:add comments

--- backend/app/Http/Controllers/BarrierController.php
+++ backend/app/Http/Controllers/BarrierController.php
@@ -11,6 +11,9 @@ class BarrierController extends Controller{
 
     public function getBarrierStatus(){
         $barrier_status = BarrierStatus::get()->first();
+        // In case the barrier status from barrier_statuses table (first record) was '0'or '1', the barrier in the 
+        // Hardware part will be respectively 'closed' or 'opened' until further notice from the admin. Otherwise, the
+        // response will be 'normal' which makes the Raspberry Pi start continue the main  required functions
         if($barrier_status->status=="0"){
             return "closed";
         }else if($barrier_status->status=="1"){
