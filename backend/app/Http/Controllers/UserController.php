b3618f62659a1ede4f648845d88fcbdfc58acb08
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-07 19:48:21 +0200

    Backend-user controller: import libraries

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -2,7 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
+use Validator;
+use App\Models\User;
+use App\Models\UserDetail;
 use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
+
+
 
 class UserController extends Controller
 {
