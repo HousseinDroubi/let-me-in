f189e1e8baa559d17ebd0ecdb3d5d0151b67308e
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-07 13:23:19 +0200

    Backend-database: implement codes table

--- backend/database/migrations/2022_11_07_112216_create_codes_table.php
+++ backend/database/migrations/2022_11_07_112216_create_codes_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('codes', function (Blueprint $table) {
             $table->id();
+            $table->integer('code');
+            $table->string('user_id');
+            $table->integer('verified')->default('0');
             $table->timestamps();
         });
     }
