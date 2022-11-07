a739df00150ebf98a563fb56a22d926a20338ca7
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-07 14:51:12 +0200

    Backend-code model:implement contents

--- backend/app/Models/Code.php
+++ backend/app/Models/Code.php
@@ -5,7 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Code extends Model
-{
+class Code extends Model{
+
     use HasFactory;
+
+    protected $fillable = [
+        'code',
+        'user_id',
+        'verified',
+    ];
+    
 }
