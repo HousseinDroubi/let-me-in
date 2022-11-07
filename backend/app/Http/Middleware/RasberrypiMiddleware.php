deb8d6eb4d5b3e1c56a1d34b5ca69c7bc0110b8f
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-07 22:55:14 +0200

    Backend-Raspberry Pi middleware:add comments

--- backend/app/Http/Middleware/RasberrypiMiddleware.php
+++ backend/app/Http/Middleware/RasberrypiMiddleware.php
@@ -14,6 +14,9 @@ class RasberrypiMiddleware
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
+
+    // In this middleware, the condition is that who want to ask for a route in the scope of Raspberry Pi must
+    // be only of id = '2'
     public function handle(Request $request, Closure $next){
         if(auth('api')->user()->id!='2')
             return redirect()->route('access-denied');
