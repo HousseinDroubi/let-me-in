a137c69f12b060e1e6aecfef83e9d21fb0b99372
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-07 13:35:17 +0200

    Backend-admin details table: add comments

--- backend/database/migrations/2022_11_07_112942_create_admin_details_table.php
+++ backend/database/migrations/2022_11_07_112942_create_admin_details_table.php
@@ -7,7 +7,9 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration{
 
     public function up(){
-
+        
+        // In this table we must have only two accounts, one for the admin, and the other
+        // for the Raspberry Pi
         Schema::create('admin_details', function (Blueprint $table) {
             $table->id();
             $table->string('user_id')->unique();
