e15dc833e2174f13f86cc0ff76c2cdbfa1e7560c
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-07 13:09:06 +0200

    Backend-users table: add comments

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,12 +6,16 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration{
 
-    public function up()
-    {
+    public function up(){
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            // The default of username is 'Unkown' since in our case, sometimes a new car plate 
+            // that's not neither in the approved list, nor in the blocked list and 
+            // we need the decision of the admin of this new user, so he can update his/her data later on.
             $table->string('username')->default('Unkown');
             $table->string('profile_url')->nullable();
+            // When user type is '0', that means it's a normal user, while when it's '1' , that means
+            // it's an admin
             $table->string('user_type')->default('0');
             $table->timestamps();
         });
