8f984691d438a72fa4a75bd54cb5d3cffd232c2c
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-07 17:49:34 +0200

    Backend-api : implement route group

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,4 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-Route::post("/register", [AuthController::class, "register"])->name("register");
+Route::group(["prefix"=> "v0.1"], function(){
+    Route::post("/register", [AuthController::class, "register"])->name("register");
+});
