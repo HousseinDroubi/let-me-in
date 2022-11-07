a7d96cc7490dc4035a5170f759bc3021b8516508
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-07 13:28:30 +0200

    Backend-codes table: add comments

--- backend/database/migrations/2022_11_07_112216_create_codes_table.php
+++ backend/database/migrations/2022_11_07_112216_create_codes_table.php
@@ -10,14 +10,18 @@ return new class extends Migration{
 
         Schema::create('codes', function (Blueprint $table) {
             $table->id();
+            // The code that will be sent to admin when he forgot his/her password
             $table->integer('code');
+            // User id is the same of id of users table of type '1'
             $table->string('user_id');
+            // The default of verified is '0' which means that the code didn't verified yet.
+            // While, verified is '1', that means the code has been verified
             $table->integer('verified')->default('0');
             $table->timestamps();
         });
     }
     public function down(){
-        
+
         Schema::dropIfExists('codes');
     }
 };
