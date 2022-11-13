6b317ccfd826ae433d2ee24a485ef83df9a9e7c9
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-13 16:07:11 +0200

    Backend-OCR: get car image path from .env file

--- backend/public/OCR/ocr.py
+++ backend/public/OCR/ocr.py
@@ -4,3 +4,8 @@ import pytesseract
 from pathlib import Path
 from dotenv import load_dotenv
 
+env_path = Path(__file__).parents[2]
+env_path = str(env_path)+"\\.env"
+load_dotenv(env_path)
+image_path = os.environ.get("CAR_PLATE_PATH")
+
