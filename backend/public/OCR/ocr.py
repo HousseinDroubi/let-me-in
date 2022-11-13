8be018a15869d00e2c1e9e4a450c48be7963622d
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-13 16:08:06 +0200

    Backend-OCR: get all characters from the car image

--- backend/public/OCR/ocr.py
+++ backend/public/OCR/ocr.py
@@ -9,3 +9,11 @@ env_path = str(env_path)+"\\.env"
 load_dotenv(env_path)
 image_path = os.environ.get("CAR_PLATE_PATH")
 
+myconfig = r"--psm 6 --oem 3"
+
+image = cv2.imread(image_path)
+car_plate_number =""
+boxes = pytesseract.image_to_boxes(image,config=myconfig)
+for box in boxes.splitlines():
+    box = box.split(" ")
+    car_plate_number = car_plate_number+box[0]
