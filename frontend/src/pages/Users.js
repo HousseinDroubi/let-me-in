d3961ecdc74ae07aa14a18c6024695768efacbe6
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-18 13:22:16 +0200

    Frontend-user page: add user states

--- frontend/src/pages/Users.js
+++ frontend/src/pages/Users.js
@@ -10,3 +10,24 @@ import PageTitle from '../components/PageTitle';
 import AddUserButton from '../components/AddUserButton';
 import PopUp from '../components/PopUp';
 
+const Users=()=> {
+    const base_url = process.env.REACT_APP_BASE_URL;
+    const myIPv4  = process.env.REACT_APP_MY_IPV4;
+  
+  const [called,setCalled]=useState(false);
+  const[users,setUsers]=useState([]);
+  const [popUpVisible,setPopupVisible]=useState(false);
+  const [id,setId]=useState('');
+  const [username,setUsername]= useState('');
+  const [carType,setCarType]= useState('');
+  const [plateNumber,setPlateNumber]= useState('');
+  const [profile,setProfile]=useState(null);
+  const [isAddingUser,setIsAddingUser]=useState(false);
+  const [defaultt,setDefaultt]=useState(false);
+  const [hasPicked,setHasPicked]=useState(false);
+  const [popupDenyVisible,setPopupDenyVisible]=useState(false);
+  const [attention,setAttention]=useState('');  
+
+
+}
+export default Users;
