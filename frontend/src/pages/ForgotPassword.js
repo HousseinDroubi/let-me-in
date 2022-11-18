d2932b01f4135354a879b2d33b8ba22c166b6a97
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-18 09:11:11 +0200

    Frontend-forgot password:import libraries

---
+++ frontend/src/pages/ForgotPassword.js
@@ -0,0 +1,10 @@
+import '../App.css';
+import NormalTitle from '../components/NormalTitle';
+import Label from '../components/Label';
+import Form from '../components/Form';
+import Button from '../components/Button';
+import PopupDeny from '../components/PopupDeny';
+
+import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import axios from 'axios';
