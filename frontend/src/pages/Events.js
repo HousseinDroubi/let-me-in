92207983306bf5f969d064124fd960448fd12f7c
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-18 10:08:59 +0200

    Frontend-events page: import libraries

---
+++ frontend/src/pages/Events.js
@@ -0,0 +1,6 @@
+import React,{useState,useEffect} from 'react';
+import NormalTitle from '../components/NormalTitle';
+import Layout from '../components/Layout';
+import PageTitle from '../components/PageTitle';
+import axios from 'axios';
+import secureLocalStorage from 'react-secure-storage';
