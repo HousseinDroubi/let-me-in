32ca8ea6be498bac557c9e05f27639f33c29f911
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-18 12:08:50 +0200

    Frontend-events page: implement a function to get events

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -18,6 +18,88 @@ const Events=()=> {
       const result = new Date(date);
       return `${monthNames[result.getMonth()]} ${date.split("-")[2]}, ${date.split("-")[0]}`;
       }
+      const getEvents = ()=>{
+
+
+        const header = {
+          headers: { Authorization: `Bearer ${secureLocalStorage.getItem("token")}` }
+        };
+          if (!called){
+               axios.get(`${base_url}get_events`,header)
+              .then(function (response) {
+                if(response.data.data.length !==0){
+                  const formatter = new Intl.DateTimeFormat("en-GB", {
+                    hour:'2-digit',
+                    minute:'2-digit',
+                    second:'2-digit',
+                    hour12: true
+                  });
+                  const array_dates = [];
+                  const sub_array_event_details=[];
+                  response.data.data.forEach(element => {
+                    const temp_array=[];
+                    array_dates.push(getMonthName(element.date));
+                    element.events.forEach(subElement=>{
+                      temp_array.push(formatter.format(Date.parse(subElement.arrival_time)));
+                      if(subElement.departure_time!=null)
+                        temp_array.push(formatter.format(Date.parse(subElement.departure_time)));
+                      else{
+                        temp_array.push(null);
+                      }  
+                      temp_array.push(subElement.difference);
+                      temp_array.push(subElement.user.username);
+                        if(subElement.user.profile_url!==null){
+                          let profile_url = subElement.user.profile_url.substring(subElement.user.profile_url.indexOf("\\let-me-in\\"));
+                          profile_url = myIPv4+profile_url;
+                          profile_url = profile_url.replace(/\\/g,"/");
+                          temp_array.push(profile_url);
+                      }else{
+                        temp_array.push(subElement.user.profile_url);
+                      }
+      
+                      temp_array.push(subElement.user.user_detail.car_type);
+                      temp_array.push(subElement.user.user_detail.car_plate_number);
+                      
+                    });
+                    sub_array_event_details.push(temp_array);
+                    
+                   
+                  });
+    
+                  const array1=[];
+                  let array2 = [];
+                  let array3=[];
+
+                  for(let i=0;i<array_dates.length;i++){
+                    let counter =0;
+                    while(counter<sub_array_event_details[i].length){
+                      array3.push(sub_array_event_details[i][counter]);
+
+                      if((counter+1)%7===0){
+                        array2.push(array3);
+                        array3=[];
+                      }
+    
+                      counter++;
+                    }
+                    array1.push(array2);
+                    array2=[];
+                    array3=[]; 
+                  }
+      
+                  setDates(array_dates);
+                  setEvents(array1);
+                }
+            })
+            .catch(function (error) {
+            });
+          
+            setCalled(true);
+          }
+         
+          }
+          useEffect( () => {getEvents();});
+        
 
 return(
     <Layout pageName='events'/>
