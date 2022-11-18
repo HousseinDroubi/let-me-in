30f87ce0abe9d28999546ce1bac043da2d05f871
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-18 13:21:36 +0200

    Frontend-user page: import libraries

---
+++ frontend/src/pages/Users.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Title from '../components/Title';
+import Layout from '../components/Layout';
+import BoxUser from '../components/BoxUser';
+import PopupDeny from '../components/PopupDeny';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import secureLocalStorage from 'react-secure-storage';
+import PageTitle from '../components/PageTitle';
+import AddUserButton from '../components/AddUserButton';
+import PopUp from '../components/PopUp';
+
