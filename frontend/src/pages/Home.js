4d51386f4454ddd8430c548308e29b5b56d2c4e0
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-18 04:11:36 +0200

    Frontend-home page: import libraries

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,9 @@
+import React,{useEffect} from 'react';
+import Button from '../components/Button';
+import { useNavigate } from "react-router-dom";
+import logo from '../assets/images/letmein-logo.png';
+import Title from '../components/Title';
+const Home=()=> {
+}
+  
+export default Home;
