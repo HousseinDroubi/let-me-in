7900d31ca8860cae8e3cbe92f2c507e908f733ba
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-18 12:28:53 +0200

    Frontend-add user button: add CSS classes

--- frontend/src/components/AddUserButton.js
+++ frontend/src/components/AddUserButton.js
@@ -2,7 +2,8 @@ import '../App.css';
 import classnames from 'classnames';
 import Add from '../assets/images/add_user_icon.png';
 const AddUserButton = (props)=>{
-    return (<button className={classnames( "add-user-button-content",props.className)} onClick={props.onClick}>
+    return (
+        <button className={classnames( "add-user-button-content",props.className)} onClick={props.onClick}>
                 <img src={Add} alt=''/>
                 <p>Add</p>
         </button>
