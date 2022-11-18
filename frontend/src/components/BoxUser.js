6d40cf66b91bc8f7691ca3d6212bdf0378a929c7
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-18 13:14:28 +0200

    Frontend-box user: implement the return view

--- frontend/src/components/BoxUser.js
+++ frontend/src/components/BoxUser.js
@@ -11,36 +11,52 @@ import BlockedImage from '../assets/images/blocked_icon.png';
 
 const BoxUser = (props)=>{
     //  props.setPopupVisible(true);
-const EditUser =()=>{
-    props.setDefaultt(false);
-    props.setIsAddingUser(false);
-    props.setId(props.id);
-    props.setUsername(props.username);
-    props.setCarType(props.car_type);
-    props.setPlateNumber(props.car_plate_number);
-    props.setProfile(props.source);
-    props.setPopupVisible(true);
-}
+    const EditUser =()=>{
+        props.setDefaultt(false);
+        props.setIsAddingUser(false);
+        props.setId(props.id);
+        props.setUsername(props.username);
+        props.setCarType(props.car_type);
+        props.setPlateNumber(props.car_plate_number);
+        props.setProfile(props.source);
+        props.setPopupVisible(true);
+    }
 
-const blockUser =()=>{
-    const header = {
-        headers: { Authorization: `Bearer ${secureLocalStorage.getItem("token")}` }
-    };
-  
-    const body = {
-        id: props.id
-    };
-  
-    axios.post(`${props.base_url}block_user`,body,header)
-        .then(function (response) {
-          console.log(response);
-          window.location.reload(false); 
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-}
+    const blockUser =()=>{
+        const header = {
+            headers: { Authorization: `Bearer ${secureLocalStorage.getItem("token")}` }
+        };
+    
+        const body = {
+            id: props.id
+        };
+    
+        axios.post(`${props.base_url}block_user`,body,header)
+            .then(function (response) {
+            console.log(response);
+            window.location.reload(false); 
+            })
+            .catch(function (error) {
+            console.log(error);
+            });
+    }
 
+    return (
+        <div className={classnames( "box",props.className)}>
 
+            <div className='box-image'>
+                <CircleImage source={props.source===null?DefaultImage:props.source}/>
+                <BoxContent name = {props.username}/>
+            </div>
+            <div className='box-details'>
+                <BoxContent name = {props.car_type}/>
+                <BoxContent name = {props.car_plate_number}/>
+            </div>
+            <div className='box-details'>
+                <BoxButton onClick={EditUser} text = 'Edit' icon={EditIcon}/>
+                <BoxButton onClick={blockUser} status = 'block' color='red' text = 'Block' icon = {BlockedImage}/>
+            </div>
+        </div>
+    );
 }
 export default BoxUser;
