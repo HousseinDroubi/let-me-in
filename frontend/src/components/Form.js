efb5ed88c8c3da820bf3cf4ad89c673fd4d73c6f
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-18 08:17:14 +0200

    Frontend-form component: implement design

---
+++ frontend/src/components/Form.js
@@ -0,0 +1,20 @@
+import '../App.css';
+import classnames from 'classnames';
+const Form = (props)=>{
+  let value='';
+    const changeState = (e) => {
+        value = e.target.value;
+        if(props.defaultt){
+          value='';
+          props.setText('');
+        }
+        else{
+          value=e.target.value;
+          props.setText(value);
+        }
+        
+      }
+ return <input type={props.type==="password"?'password':'text'} className={classnames('form')} onChange={changeState}  value={props.defaultt?'':props.text}/>
+    
+}
+export default Form;
