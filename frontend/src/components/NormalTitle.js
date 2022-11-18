75fedb07ee8d28b8afb3fc6590934009e2573080
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-18 04:03:20 +0200

    Frontend-normal title: add CSS classes

--- frontend/src/components/NormalTitle.js
+++ frontend/src/components/NormalTitle.js
@@ -2,6 +2,6 @@ import '../App.css';
 import classnames from 'classnames';
 const NormalTitle = (props)=>{
     let title = props.title;
-    return <p className={classnames( "title",props.className)}>{title}</p>
+    return <p className={classnames( "normal-title",props.className)}>{title}</p>
 }
 export default NormalTitle;
