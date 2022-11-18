362ec647cfe17c0eef0eaa710c71bf930246102a
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-18 15:43:37 +0200

    Frontend-admin profile: import libraries

---
+++ frontend/src/pages/AdminProfile.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import Button from '../components/Button';
+import Label from '../components/Label';
+import Form from '../components/Form';
+import { useNavigate } from "react-router-dom";
+import Title from '../components/Title';
+import Layout from '../components/Layout';
+import LargeImage from '../components/LargeImage';
+import secureLocalStorage from 'react-secure-storage';
+import DefaultImage from '../assets/images/default_image.png';
+import PopupPassword from '../components/PopupPassword';
+import PopupDeny from '../components/PopupDeny';
+import axios from 'axios';
+
+const AdminProfile=()=> {
+
+}
+  
+export default AdminProfile;
