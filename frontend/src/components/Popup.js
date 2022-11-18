45b6d45b2a6338f44b9198b669027feb3849233f
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-18 12:37:57 +0200

    Frontend-popup component: implement a function to add or update user

--- frontend/src/components/Popup.js
+++ frontend/src/components/Popup.js
@@ -43,5 +43,77 @@ const PopUp = (props)=>{
         catch(e){
         }
       }
+
+      const addOrUpdateUser=()=>{
+        if(props.isAddingUser){
+            if(props.username.length<3 || props.username.length>20){
+              showDenyPopUp("Username must be between 3 and 20 characters");
+            }else if(props.carType.length<3 || props.carType.length>20){
+              showDenyPopUp("Car type must be between 3 and 20 characters");
+            }else if(props.plateNumber.length<2 || props.plateNumber.length>7){
+              showDenyPopUp("Car plate number must be between 2 and 7 characters");
+            }
+            else{
+              
+                const body={
+                    username:props.username,
+                    car_type:props.carType,
+                    car_plate_number:props.plateNumber
+                };
+                if(props.hasPicked){
+                    body.profile_url = props.profile.split(',')[1].replace(/"/g,"/");
+                }
+
+                const header = {
+                    headers: { Authorization: `Bearer ${secureLocalStorage.getItem("token")}` }
+                };
+    
+                axios.post(`${base_url}add_user`,body,header)
+                .then(function (response) {
+                window.location.reload(false); 
+                })
+                .catch(function (error) {
+                showDenyPopUp("Try with another car plate number.");
+                });
+
+
+
+            }
+        }
+        
+            else{
+            if(props.username.length<3 || props.username.length>20){
+                showDenyPopUp("Username must be between 3 and 20 characters");
+            }else if(props.carType.length<3 || props.carType.length>20){
+                showDenyPopUp("Car type must be between 3 and 20 characters");
+            }else if(props.plateNumber.length<2 || props.plateNumber.length>7){
+                showDenyPopUp("Car plate number must be between 2 and 7 characters");
+            }
+            else{
+                const body={
+                    id:props.id,
+                    username:props.username,
+                    car_type:props.carType,
+                    car_plate_number:props.plateNumber
+                };
+                const header = {
+                    headers: { Authorization: `Bearer ${secureLocalStorage.getItem("token")}` }
+                };
+                if(props.hasPicked){
+                    body.profile_url = props.profile.split(',')[1].replace(/"/g,"/");
+                }
+          
+
+                axios.post(`${base_url}edit_user`,body,header)
+                .then(function (response) {
+                window.location.reload(false); 
+                })
+                .catch(function (error) {
+                showDenyPopUp("Something went wrong.");
+                });
+
+                }}
+            }
+
 }
 export default PopUp;
