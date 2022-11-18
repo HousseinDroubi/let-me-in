998fadb7bc537ac27a768b995a392ad9b21a9a6b
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-18 10:39:33 +0200

    Frontend-page title component: add CSS files

--- frontend/src/components/PageTitle.js
+++ frontend/src/components/PageTitle.js
@@ -1,8 +1,6 @@
 import '../App.css';
 import classnames from 'classnames';
 const PageTitle = (props)=>{
-
-    // return <button className="button">{props.name}</button>
     return <p className={classnames( "page-title-content",props.className)}>{props.text}</p>
     
 }
