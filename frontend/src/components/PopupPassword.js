1ac93cd53d9f764783448825dcf8de4a7bd78368
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-18 15:55:59 +0200

    Frontend-popup password: implement method to change password

--- frontend/src/components/PopupPassword.js
+++ frontend/src/components/PopupPassword.js
@@ -19,5 +19,48 @@ const PopupPassword = (props)=>{
     const closePopup=()=>{
         props.setPopupVisible(false);
     }
+
+    const changePassword = ()=>{
+        if(props.oldPassword==='')
+          showDenyPopUp("Old password should not be empty.");
+        else if(props.newPassword==='')
+          showDenyPopUp("Please enter your password.");
+        else if(props.confirmPassword==='')
+          showDenyPopUp("Please re-enter your password.");
+        else if((props.newPassword.length<5 || props.newPassword.length>30) || (props.confirmPassword.length<5 || props.confirmPassword.length>30))
+        showDenyPopUp("Password must be between 5 and 30 characters.");
+        else if(props.newPassword!==props.confirmPassword) 
+          showDenyPopUp("Passwords not match."); 
+        else if(props.oldPassword===props.newPassword){
+          showDenyPopUp("Already same password!!"); 
+        }  
+        else{  
+        
+            const body={
+                old_password:props.oldPassword,
+                new_password:props.newPassword
+            };
+            
+            const header = {
+                headers: { Authorization: `Bearer ${secureLocalStorage.getItem("token")}` }
+            };
+                
+                    axios.post(`${base_url}change_admin_password`,body,header)
+                    .then(function (response) {
+                    if(response.data.message==='Wrong Password'){
+                        
+                        showDenyPopUp("Wrong Password."); 
+                    }else{
+                        showDenyPopUp("Password changed successfully"); 
+                        props.setOldPassword("");
+                        props.setNewPassword("");
+                        props.setConfirmPassword("");
+                    }
+                })
+                .catch(function (error) {
+                    showDenyPopUp("Something went wrong."); 
+                });
+        }
+    }
 }
 export default PopupPassword;
