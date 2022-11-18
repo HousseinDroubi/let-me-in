1eead39d7ddd9334e974a5866b2513828e7cd5d8
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-18 04:13:12 +0200

    Frontend-home page: implement design

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,8 +2,30 @@ import React,{useEffect} from 'react';
 import Button from '../components/Button';
 import { useNavigate } from "react-router-dom";
 import logo from '../assets/images/letmein-logo.png';
-import Title from '../components/Title';
+import NormalTitle from '../components/NormalTitle';
 const Home=()=> {
+    const navigate = useNavigate();
+
+    function navigateToLogin(){
+    navigate('/login');
+    }
+    const isLoggedIn=()=>{
+    if(localStorage.getItem("remember_me")==='checked')
+        navigate('/events');
+    }
+    useEffect( () => {
+    isLoggedIn();
+    
+    });
+    return (
+      
+        <div className='home'>
+        
+          <NormalTitle title="Let me in"/>
+          <img src={logo} alt="logo" className='image-logo'/>
+          <Button  name={"Get Started"} onClick={navigate}  className='mt-80 mb-30'></Button>
+        </div>
+      );
 }
   
 export default Home;
