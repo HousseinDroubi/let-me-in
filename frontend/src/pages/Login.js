5adb0f2947b8e69efefe88df16666c8384eb6432
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-18 07:58:35 +0200

    Frontend-login: implement a function to check entries

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -32,4 +32,57 @@ const Login=()=> {
     const navigateToEvents=()=>{
         navigate('/events');
     }
+
+    const  checkEmailAndPassword = ()=>{
+        if(email===''){
+          showDenyPopUp("Email should not be empty.");
+        }else if(!email.includes("@")){
+          showDenyPopUp("Invalid email.");
+        }
+        else{
+        const body = {
+          email: email,
+          password: password
+      };
+      
+         axios.post(`${base_url}login`, body)
+          .then(function (response) {
+            if(response.status===200){
+              localStorage.clear();
+              secureLocalStorage.clear();
+              secureLocalStorage.setItem("token",response.data.access_token);
+              const header = {
+                headers: { Authorization: `Bearer ${secureLocalStorage.getItem("token")}` }
+            };
+                  axios.get(`${base_url}get_barrier_status`,header)
+                  .then(function (response) {
+                        localStorage.setItem("barrier_status",response.data);
+                  })
+                  .catch(function (error) {
+                    showDenyPopUp("Someting went wrong.");
+                  });
+      
+                   axios.get(`${base_url}get_admin_data`,header)
+                  .then(function (response) {
+                    secureLocalStorage.setItem("email",response.data.email);
+                    secureLocalStorage.setItem("username",response.data.user.username);
+                    let profile_url = response.data.user.profile_url.substring(response.data.user.profile_url.indexOf("\\let-me-in\\"));
+                    profile_url = myIPv4+profile_url;
+                    profile_url = profile_url.replace(/\\/g,"/");
+                    localStorage.setItem("profile_url",profile_url);
+                  })
+                  .catch(function (error) {
+                    showDenyPopUp("Someting went wrong.");
+                  });
+                  navigateToEvents();
+            }
+      
+          })
+          .catch(function (error) {
+            showDenyPopUp("Wrong email or password.");
+          });
+      }}
 }
+
+  
+export default Login;
