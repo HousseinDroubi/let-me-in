5c6ee5453b9d8f1acf4c493e3bb2d78337316537
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-18 13:29:51 +0200

    Frontend: add users route to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Events from './pages/Events';
 import ForgotPassword from './pages/ForgotPassword';
+import Users from './pages/Users';
 // require('dotenv').config()
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route exact path='/login' element={<Login/>}/>
         <Route exact path='/forgot_password' element={<ForgotPassword/>}/>
         <Route exact path='/events' element={<Events/>}/>
+        <Route exact path='/events' element={<Users/>}/>
       </Routes>
     </Router>
   );
