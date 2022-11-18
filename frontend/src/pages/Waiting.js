1bb0b9d20db150a05fc2d9ebb2e776fec4bd4c4f
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-18 15:10:48 +0200

    Frontend-wating page: import libraries

---
+++ frontend/src/pages/Waiting.js
@@ -0,0 +1,19 @@
+import React,{useEffect, useState} from 'react';
+import NormalTitle from '../components/NormalTitle';
+import DrawerButton from '../components/DrawerButton';
+import Layout from '../components/Layout';
+import secureLocalStorage from 'react-secure-storage';
+import axios from 'axios';
+import waitingProfile from '../assets/images/waiting_profile.png';
+import Label from '../components/Label';
+import Form from '../components/Form';
+import TextView from '../components/TextView';
+import DecisionButton from '../components/DecisionButton';
+import LargeImage from '../components/LargeImage';
+import PopupDeny from '../components/PopupDeny';
+
+const Waiting=()=> {
+
+}
+  
+export default Waiting;
