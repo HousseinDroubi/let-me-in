bff33ffb46e2223913ffb35a6638ed92c16fbcb6
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-18 12:34:40 +0200

    Frontend-popup component: implement a function to show the deny popup

--- frontend/src/components/Popup.js
+++ frontend/src/components/Popup.js
@@ -1,7 +1,6 @@
 import '../App.css';
 import classnames from 'classnames';
 import AcceptIcon from '../assets/images/accept.png';
-
 import NormalTitle from '../components/NormalTitle';
 import CircleImage from '../components/CircleImage';
 import DefaultImage from  '../assets/images/default_image.png';
@@ -11,3 +10,12 @@ import BoxButton from '../components/BoxButton';
 import { useState } from 'react';
 import secureLocalStorage from 'react-secure-storage';
 import axios from 'axios';  
+
+const PopUp = (props)=>{
+    const base_url = process.env.REACT_APP_BASE_URL;  
+    const showDenyPopUp = (att)=>{
+      props.setPopupDenyVisible(true);
+      props.setAttention(att);
+    }
+}
+export default PopUp;
