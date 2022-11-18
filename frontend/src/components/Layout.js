b021ab0cd1a06bd19edd93b7dda3cec2b89b2b04
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-18 09:46:02 +0200

    Frontend-layout component: add use states

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -8,3 +8,14 @@ import axios from 'axios';
 import { useNavigate } from "react-router-dom";
 import secureLocalStorage from 'react-secure-storage';
 import ModeTitle from './ModeTitle';
+
+const Layout = (props)=>{
+
+    const navigate = useNavigate();
+    const base_url = process.env.REACT_APP_BASE_URL;
+    const [isTurnedOnBlue,setIsTurnedOnBlue] = useState(false);
+    const [isTurnedOnGreen,setIsTurnedOnGreen] = useState(false);
+    const [isTurnedOnRed,setIsTurnedOnRed] = useState(false);
+
+}
+export default Layout;
