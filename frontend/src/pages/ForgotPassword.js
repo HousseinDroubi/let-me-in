f754af21e3b95764ee3cc9c69e1a37bbd115989c
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-18 09:11:45 +0200

    Frontend-forgot password:add use states

--- frontend/src/pages/ForgotPassword.js
+++ frontend/src/pages/ForgotPassword.js
@@ -8,3 +8,17 @@ import PopupDeny from '../components/PopupDeny';
 import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
+const ForgotPassword=()=> {
+    const navigate = useNavigate();
+    const [text,setText]=useState("Send code");
+    const [email,setEmail]=useState('');
+    const [code,setCode]=useState('');
+    const [password,setPassword]=useState('');
+    const [rePassword,setRePassword]=useState('');
+    const [isEmailVerified,setIsEmailVerified]=useState(false);
+    const [isCodeVerified,setIsCodeVerified]=useState(false);
+    const [popupDenyVisible,setPopupDenyVisible]=useState(false);
+    const [attention,setAttention]=useState('');
+    const base_url =process.env.REACT_APP_BASE_URL;
+
+}
