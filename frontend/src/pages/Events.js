64a236866391c3bb4d97eaed5116b379a18fb235
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-18 11:22:43 +0200

    Frontend-events page: add use states

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -5,6 +5,12 @@ import PageTitle from '../components/PageTitle';
 import axios from 'axios';
 import secureLocalStorage from 'react-secure-storage';
 const Events=()=> {
+    const myIPv4  = process.env.REACT_APP_MY_IPV4;
+    const base_url = process.env.REACT_APP_BASE_URL;
+    const [called,setCalled]=useState(false);
+    const[dates,setDates]=useState([]);
+    const[events,setEvents]=useState([]);
+    
 return(
     <Layout pageName='events'/>
 );
