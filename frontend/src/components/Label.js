20370615d79dc08111f0e2ceaad55a5e579d1248
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-20 02:02:43 +0200

    Frontend-login page: add media query

--- frontend/src/components/Label.js
+++ frontend/src/components/Label.js
@@ -1,7 +1,6 @@
 import '../App.css';
 import classnames from 'classnames';
 const Label = (props)=>{
-    let title = props.title;
-    return <p  className={classnames( "label",props.className)}>{title}</p>
+    return <p  className={classnames( "label",props.className)}>{props.title}</p>
 }
 export default Label;
