01cab5a17227673ce1bdd2c385ea94f52f87870a
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-21 00:26:19 +0200

    Frontend-layout toggle component: implement the design

---
+++ frontend/src/components/LayoutToggle.js
@@ -0,0 +1,11 @@
+import '../App.css';
+import classnames from 'classnames';
+import layoutToggleIcon from '../assets/images/layout-toggle-icon.png';
+const LayoutToggle = (props)=>{
+
+    return (
+    <div className='layout-toggle'>
+    <img src={layoutToggleIcon} className={classnames( "layout-toggle-icon",props.className)} onClick={props.onClick} alt=""/>
+    </div>);
+}
+export default LayoutToggle;
