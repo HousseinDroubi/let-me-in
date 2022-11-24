17af2fe7356b0d352b1b16bdcd4b6ecc44326b87
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-24 22:03:24 +0200

    Backend-users table: clean code

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -10,8 +10,8 @@ return new class extends Migration{
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             // The default of username is 'Unkown' since in our case, sometimes a new car plate 
-            // that's not neither in the approved list, nor in the blocked list and 
-            // we need the decision of the admin of this new user, so he can update his/her data later on.
+            // that's neither in the approved list, nor in the blocked list and we need the decision
+            // of the admin of this new user, so, admin can update that user's data later on.
             $table->string('username')->default('Unkown');
             $table->string('profile_url')->nullable();
             // When user type is '0', that means it's a normal user, while when it's '1' , that means
