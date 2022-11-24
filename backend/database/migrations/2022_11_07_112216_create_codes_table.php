7dc9b9bce1b435f851f83f3619eaac762d7c9569
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-24 22:06:19 +0200

    Backend-codes table: clean code

--- backend/database/migrations/2022_11_07_112216_create_codes_table.php
+++ backend/database/migrations/2022_11_07_112216_create_codes_table.php
@@ -9,10 +9,11 @@ return new class extends Migration{
     public function up(){
 
         Schema::create('codes', function (Blueprint $table) {
+
             $table->id();
-            // The code that will be sent to admin when he forgot his/her password
+            // The code that will be sent to admin when he/she forgot his/her password
             $table->integer('code');
-            // User id is the same of id of users table of type '1'
+            // user_id is the same of id of users table of type '1'
             $table->string('user_id');
             // The default of verified is '0' which means that the code didn't verified yet.
             // While, verified is '1', that means the code has been verified
