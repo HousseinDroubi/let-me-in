57e4abf5b74f844d90ff9f7bca4fa248c3b7b7cf
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-25 21:01:58 +0200

    Frontend-circle image component: clean code

--- frontend/src/components/CircleImage.js
+++ frontend/src/components/CircleImage.js
@@ -6,5 +6,6 @@ const CircleImage = (props) => {
     return (
         <img src={props.source} alt="name" className={classnames( "circle-image",props.className)} onClick={props.onClick}/>
     );
+    
   };
 export default CircleImage;
