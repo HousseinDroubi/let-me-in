c916017063327568f4f3b4796ee5b0cc31533514
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-25 22:15:39 +0200

    Frontend-users page: clean code

--- frontend/src/pages/Users.js
+++ frontend/src/pages/Users.js
@@ -29,43 +29,45 @@ const Users=()=> {
   const [popupDenyVisible,setPopupDenyVisible]=useState(false);
   const [attention,setAttention]=useState('');  
   const [isDrawerVisible,setIsDrawerVisible]=useState(true);
+
     // Here, we are getting the users who aren't blocked (has status = '0')
   const getUsers = ()=>{
 
     const header = {
       headers: { Authorization: `Bearer ${secureLocalStorage.getItem("token")}` }
     };
-      if (!called){
-           axios.get(`${base_url}users`,header)
-           .then(function (response) {
-            if(response.data.data.length !==0){
-              let array = [];
-              response.data.data.forEach(element => {
-                const sub_array = [];
-                sub_array.push(element.id);
-                sub_array.push(element.username);
-                if(element.profile_url!==null){
-                    let profile_url = element.profile_url.substring(element.profile_url.indexOf("\\let-me-in\\"));
-                    profile_url = myIPv4+profile_url;
-                    profile_url = profile_url.replace(/\\/g,"/");
-                    sub_array.push(profile_url);
-                }else{
-                    sub_array.push(element.profile_url);
-                }
-                sub_array.push(element.user_detail.car_type);
-                sub_array.push(element.user_detail.car_plate_number);
-                array.push(sub_array);
-              });
-            setUsers(array);
-            }
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-      
-        setCalled(true);
-      }
-      }
+
+    if (!called){
+          axios.get(`${base_url}users`,header)
+          .then(function (response) {
+          if(response.data.data.length !==0){
+            let array = [];
+            response.data.data.forEach(element => {
+              const sub_array = [];
+              sub_array.push(element.id);
+              sub_array.push(element.username);
+              if(element.profile_url!==null){
+                  let profile_url = element.profile_url.substring(element.profile_url.indexOf("\\let-me-in\\"));
+                  profile_url = myIPv4+profile_url;
+                  profile_url = profile_url.replace(/\\/g,"/");
+                  sub_array.push(profile_url);
+              }else{
+                  sub_array.push(element.profile_url);
+              }
+              sub_array.push(element.user_detail.car_type);
+              sub_array.push(element.user_detail.car_plate_number);
+              array.push(sub_array);
+            });
+          setUsers(array);
+          }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    
+      setCalled(true);
+    }
+  }
       useEffect( () => {getUsers();});
 
       const showPopup = ()=>{
@@ -81,14 +83,17 @@ const Users=()=> {
       }
 
       const togglelayoutVisibility =()=>{
+
         if(isDrawerVisible){
             setIsDrawerVisible(false);
         }else{
             setIsDrawerVisible(true);
         }
+
     }
 
       if(users.length!==0){
+
         return (
           <> 
             <div className='land display-none'>
@@ -96,23 +101,20 @@ const Users=()=> {
                 <Popup className={popUpVisible?'visiblity-visible':'visiblity-hidden'} setPopupVisible={setPopupVisible} id={id} username={username} carType={carType} plateNumber={plateNumber} profile={profile} hasPicked={hasPicked} setUsername={setUsername} setCarType={setCarType} setPlateNumber={setPlateNumber} setProfile={setProfile} isAddingUser={isAddingUser} setIsAddingUser={setIsAddingUser} setDefaultt={setDefaultt} setHasPicked={setHasPicked} setPopupDenyVisible={setPopupDenyVisible} attention={attention} setAttention={setAttention}/>
                 <PopupDeny className={popupDenyVisible?'visiblity-visible':'visiblity-hidden'} setPopupDenyVisible={setPopupDenyVisible} attention={attention}/>
                 <div className='land-content'>
-                <LayoutToggle onClick={togglelayoutVisibility} className='position-static'/>
+                    <LayoutToggle onClick={togglelayoutVisibility} className='position-static'/>
                     <div className='page-title page-title-users'>
                         <PageTitle text='All Users'/>
                         <AddUserButton onClick={showPopup}/>
                     </div>
-
                     {users.map((element,index)=>{
                         return (
                             <div className='box-container' key={index}>
                             <BoxUser id = {element[0]} username={element[1]} source ={element[2]} car_type={element[3]} status='block' car_plate_number={element[4]} base_url={base_url} element={element} setPopupVisible={setPopupVisible} setUsername={setUsername} setCarType={setCarType} setPlateNumber={setPlateNumber} setProfile={setProfile} setIsAddingUser={setIsAddingUser} setDefaultt={setDefaultt} defaultt={defaultt} setId={setId}/>
                             </div> 
-                            );
+                        );
                     })}
-
                 </div>
             </div>
-        
           </>
         );
       }else{
