549606817ac3c896be744a421a30202c678ae7a8
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-25 21:30:06 +0200

    Frontend-small text component: clean code

--- frontend/src/components/SamllText.js
+++ frontend/src/components/SamllText.js
@@ -1,7 +1,11 @@
 import '../App.css';
 import classnames from 'classnames';
+
 const SmallText = (props)=>{
-    return <p className={classnames( "small-text",props.className)} onClick={props.onClick}>{props.text}</p>
+    
+    return (
+        <p className={classnames( "small-text",props.className)} onClick={props.onClick}>{props.text}</p>
+    );    
     
 }
 export default SmallText;
