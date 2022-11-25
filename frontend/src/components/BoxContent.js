72e9e97eb60f337271390b58e49d013ee771de7a
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-25 20:58:20 +0200

    Frontend-box content component: clean code

--- frontend/src/components/BoxContent.js
+++ frontend/src/components/BoxContent.js
@@ -1,7 +1,9 @@
 import '../App.css';
 import classnames from 'classnames';
+
 const BoxContent = (props)=>{
+
     return <p className={classnames( "box-content",props.className)}>{props.name}</p>
-    
+
 }
 export default BoxContent;
