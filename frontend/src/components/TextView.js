7dcb4662c37b32719d00b859cfa37e0db2146498
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-25 21:30:48 +0200

    Frontend-text view component: clean code

--- frontend/src/components/TextView.js
+++ frontend/src/components/TextView.js
@@ -1,8 +1,11 @@
 import '../App.css';
 import classnames from 'classnames';
+
 const TextView = (props)=>{
 
-    return <p className={classnames( "form text-view",props.className)}>{props.text}</p>
+    return (
+         <p className={classnames( "form text-view",props.className)}>{props.text}</p>
+    );     
    
 }
 export default TextView;
