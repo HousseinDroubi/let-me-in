b24981794baaf45b689e9e00bdf8cd031ca2230c
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-25 21:37:58 +0200

    Frontend-blocked users page: clean code

--- frontend/src/pages/BlockedUsers.js
+++ frontend/src/pages/BlockedUsers.js
@@ -9,6 +9,7 @@ import BlockedIcon from '../assets/images/blocked_icon.png';
 import LayoutToggle from '../components/LayoutToggle';
 
 const BlockedUsers= ()=> {
+
     const myIPv4  = process.env.REACT_APP_MY_IPV4;
     const base_url = process.env.REACT_APP_BASE_URL;
     const[called,setCalled]=useState(false);
@@ -21,56 +22,60 @@ const BlockedUsers= ()=> {
         const header = {
           headers: { Authorization: `Bearer ${secureLocalStorage.getItem("token")}` }
         };
+
         if (!called){
             axios.get(`${base_url}blocked_users`,header)
             .then(function (response) {
-            if(response.data.data.length !==0){
+                if(response.data.data.length !==0){
 
-                // Defining this array to push other sub arrays, then display them using map
-                let array = [];
-                response.data.data.forEach(element => {
-                const sub_array = [];
-                sub_array.push(element.id);
-                sub_array.push(element.username);
+                    // Defining this array to push other sub arrays, then display them using map
+                    let array = [];
+                    response.data.data.forEach(element => {
+                    const sub_array = [];
+                    sub_array.push(element.id);
+                    sub_array.push(element.username);
 
-                // Here, we are replacing the image path at the first by our IPv4
-                if(element.profile_url!==null){
-                    let profile_url = element.profile_url.substring(element.profile_url.indexOf("\\let-me-in\\"));
-                    profile_url = myIPv4+profile_url;
-                    profile_url = profile_url.replace(/\\/g,"/");
-                    sub_array.push(profile_url);
-                }else{
-                sub_array.push(element.profile_url);
+                    // Here, we are replacing the image path at the first by our IPv4
+                    if(element.profile_url!==null){
+                        let profile_url = element.profile_url.substring(element.profile_url.indexOf("\\let-me-in\\"));
+                        profile_url = myIPv4+profile_url;
+                        profile_url = profile_url.replace(/\\/g,"/");
+                        sub_array.push(profile_url);
+                    }else{
+                        sub_array.push(element.profile_url);
+                    }
+
+                    sub_array.push(element.user_detail.car_type);
+                    sub_array.push(element.user_detail.car_plate_number);
+                    array.push(sub_array);
+                    });
+
+                    setBlockedUsers(array);
                 }
-                sub_array.push(element.user_detail.car_type);
-                sub_array.push(element.user_detail.car_plate_number);
-                array.push(sub_array);
-                });
-
-                setBlockedUsers(array);
-            }
-        })
+            })
         .catch(function (error) {
-
         });
         
         setCalled(true);
-          }
+        }
          
-          }
-          useEffect( () => {getBlockedUsers();});
+    }
+
+    useEffect( () => {getBlockedUsers();});
 
 
     const togglelayoutVisibility =()=>{
+
         if(isDrawerVisible){
             setIsDrawerVisible(false);
         }else{
             setIsDrawerVisible(true);
         }
+
     }
 
 
-        if(blockedUsers.length!==0){
+    if(blockedUsers.length!==0){
     
         return(
             <div className='land'>
@@ -105,7 +110,7 @@ const BlockedUsers= ()=> {
         
                     return (
                     <div className='box-container' key={index}>
-                    <Box  id={element[0]} username={element[1]} source={element[2]} car_type={element[3]} car_plate_number={element[4]} icon={BlockedIcon} onClick={blockUser}/>
+                        <Box  id={element[0]} username={element[1]} source={element[2]} car_type={element[3]} car_plate_number={element[4]} icon={BlockedIcon} onClick={blockUser}/>
                     </div> 
                     )
                 } 
@@ -114,9 +119,8 @@ const BlockedUsers= ()=> {
                 </div>
             </div>
         );
-        }
-        
-        else{
+    }
+    else{
         
         return(
             <div className='land'>
@@ -128,7 +132,7 @@ const BlockedUsers= ()=> {
             
             </div>
             );
-        }      
+    }      
 
 }
 export default BlockedUsers;
