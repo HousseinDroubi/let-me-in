41e5893a8d5d07299163a2f68c7464cbbfbf1028
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-25 21:18:36 +0200

    Frontend-layout component: clean code

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -18,6 +18,7 @@ const Layout = (props)=>{
     const [isTurnedOnRed,setIsTurnedOnRed] = useState(false);
 
     const getBarrierStatus= ()=>{
+
         const barrier_status= localStorage.getItem("barrier_status");
             if(barrier_status==='normal'){
                 setIsTurnedOnBlue(true);
@@ -29,24 +30,27 @@ const Layout = (props)=>{
                 setIsTurnedOnRed(true);
             }
     }
+
     useEffect(() => {getBarrierStatus();
       });
 
       const changeBarrierStatus=(status)=>{
+
         const header = {
             headers: { Authorization: `Bearer ${secureLocalStorage.getItem("token")}` }
         };
+
         const body={
             status:status
         };
-            axios.post(`${base_url}change_barrier_status`,body,header)
-            .then(function (response) {
-            console.log(response);
 
-            })
-            .catch(function (error) {
+        axios.post(`${base_url}change_barrier_status`,body,header)
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
             console.log(error);
-            });
+        });
     }   
 
     const turnOfCircles = ()=>{
@@ -55,75 +59,87 @@ const Layout = (props)=>{
         setIsTurnedOnRed(false);
     }
     const blueToggeled= ()=>{
+
         if(!isTurnedOnBlue){
-         changeBarrierStatus(2);
-        localStorage.setItem("barrier_status","normal");
-        turnOfCircles();
-        setIsTurnedOnBlue(true);
+            changeBarrierStatus(2);
+            localStorage.setItem("barrier_status","normal");
+            turnOfCircles();
+            setIsTurnedOnBlue(true);
         }
+
     }
     const greenToggeled= ()=>{
+
         if(!isTurnedOnGreen){
-         changeBarrierStatus(1);
-        localStorage.setItem("barrier_status","opened");
-        turnOfCircles();
-        setIsTurnedOnGreen(true);
-    }
+            changeBarrierStatus(1);
+            localStorage.setItem("barrier_status","opened");
+            turnOfCircles();
+            setIsTurnedOnGreen(true);
+        }
+
     }
     const redToggeled= ()=>{
+
         if(!isTurnedOnRed){
-         changeBarrierStatus(0);
-        localStorage.setItem("barrier_status","closed");
-        turnOfCircles();
-        setIsTurnedOnRed(true);
+            changeBarrierStatus(0);
+            localStorage.setItem("barrier_status","closed");
+            turnOfCircles();
+            setIsTurnedOnRed(true);
         }
+
     }
 
     const navigateToEvents = ()=>{
         navigate('/events');
     }
+
     const navigateToUsers = ()=>{
         navigate('/users');
     }
+
     const navigateToBlocked = ()=>{
         navigate('/blocked');
     }
+
     const navigateToWaiting = ()=>{
         navigate('/waiting');
     }
+
     const navigateToProfile=()=>{
         navigate('/admin_profile');
     }
 
     const togglelayoutVisibility =()=>{
+
         if(props.isDrawerVisible){
             props.setIsDrawerVisible(false);
         }else{
             props.setIsDrawerVisible(true);
         }
+
     }
 
     return (
         <div className={props.isDrawerVisible?'display-block':'display-none'}>
-        <div className='drawer'>
-            <LayoutToggle onClick={togglelayoutVisibility}/>
-            <ModeTitle title= {'Modes'}/>
-            <div className='modes-circles'>
-                <Circle isTurnedOn={isTurnedOnBlue} turnedOnStyle='backgorund-mode-blue' turnedOffStyle='backgorund-border-mode-blue' onClick={blueToggeled} title="Normal"/>
-                <Circle isTurnedOn={isTurnedOnGreen} turnedOnStyle='backgorund-mode-green' turnedOffStyle='backgorund-border-mode-green' onClick={greenToggeled} title="Open"/>
-                <Circle isTurnedOn={isTurnedOnRed} turnedOnStyle='backgorund-mode-red' turnedOffStyle='backgorund-border-mode-red' onClick={redToggeled} title="Close"/>
+            <div className='drawer'>
+                <LayoutToggle onClick={togglelayoutVisibility}/>
+                <ModeTitle title= {'Modes'}/>
+                <div className='modes-circles'>
+                    <Circle isTurnedOn={isTurnedOnBlue} turnedOnStyle='backgorund-mode-blue' turnedOffStyle='backgorund-border-mode-blue' onClick={blueToggeled} title="Normal"/>
+                    <Circle isTurnedOn={isTurnedOnGreen} turnedOnStyle='backgorund-mode-green' turnedOffStyle='backgorund-border-mode-green' onClick={greenToggeled} title="Open"/>
+                    <Circle isTurnedOn={isTurnedOnRed} turnedOnStyle='backgorund-mode-red' turnedOffStyle='backgorund-border-mode-red' onClick={redToggeled} title="Close"/>
+                </div>
+                <div className='layout-buttons'>
+                    <LayoutButton name={"Home"} onClick={navigateToEvents} className={props.pageName==='events'?'button-drawer-background-blue':'button-drawer-background-navy'}/>
+                    <LayoutButton name={"All Users"} onClick={navigateToUsers} className={props.pageName==='users'?'button-drawer-background-blue':'button-drawer-background-navy'}/>
+                    <LayoutButton name={"Blocked List"} onClick={navigateToBlocked} className={props.pageName==='blocked'?'button-drawer-background-blue':'button-drawer-background-navy'}/>
+                    <LayoutButton name={"Waiting"} onClick={navigateToWaiting} className={props.pageName==='waiting'?'button-drawer-background-blue':'button-drawer-background-navy'}/>
+                </div>
+                <div className='layout-info' onClick = {navigateToProfile}>
+                    <CircleImage source={localStorage.getItem("profile_url")}/>
+                    <ModeTitle title={secureLocalStorage.getItem("username")}/>
+                </div>
             </div>
-            <div className='layout-buttons'>
-            <LayoutButton name={"Home"} onClick={navigateToEvents} className={props.pageName==='events'?'button-drawer-background-blue':'button-drawer-background-navy'}/>
-            <LayoutButton name={"All Users"} onClick={navigateToUsers} className={props.pageName==='users'?'button-drawer-background-blue':'button-drawer-background-navy'}/>
-            <LayoutButton name={"Blocked List"} onClick={navigateToBlocked} className={props.pageName==='blocked'?'button-drawer-background-blue':'button-drawer-background-navy'}/>
-            <LayoutButton name={"Waiting"} onClick={navigateToWaiting} className={props.pageName==='waiting'?'button-drawer-background-blue':'button-drawer-background-navy'}/>
-            </div>
-            <div className='layout-info' onClick = {navigateToProfile}>
-                <CircleImage source={localStorage.getItem("profile_url")}/>
-                <ModeTitle title={secureLocalStorage.getItem("username")}/>
-            </div>
-        </div>
         </div>
         );
 
