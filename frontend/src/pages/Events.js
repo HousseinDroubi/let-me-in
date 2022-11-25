2531305e85b0658ef680ffd6ad01b1c98a941677
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-25 21:40:07 +0200

    Frontend-events page: clean code

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -8,6 +8,7 @@ import secureLocalStorage from 'react-secure-storage';
 import LayoutToggle from '../components/LayoutToggle';
 
 const Events=()=> {
+
     const myIPv4  = process.env.REACT_APP_MY_IPV4;
     const base_url = process.env.REACT_APP_BASE_URL;
     const [called,setCalled]=useState(false);
@@ -17,11 +18,13 @@ const Events=()=> {
 
     // In the below function, we are changing the from month number to its name
     const getMonthName = (date)=>{
+      
         const monthNames = ["Jan", "Feb", "March", "April", "May", "June",
         "July", "Aug", "Sep", "Oct", "Nov", "Dec"
       ];  
       const result = new Date(date);
       return `${monthNames[result.getMonth()]} ${date.split("-")[2]}, ${date.split("-")[0]}`;
+
       }
 
       // In the below method we are getting the events happened by the users. Each event contains
@@ -31,121 +34,126 @@ const Events=()=> {
         const header = {
           headers: { Authorization: `Bearer ${secureLocalStorage.getItem("token")}` }
         };
-          if (!called){
-               axios.get(`${base_url}get_events`,header)
-              .then(function (response) {
-                if(response.data.data.length !==0){
-                  const formatter = new Intl.DateTimeFormat("en-GB", {
-                    hour:'2-digit',
-                    minute:'2-digit',
-                    second:'2-digit',
-                    hour12: true
-                  });
-                  const array_dates = [];
-                  const sub_array_event_details=[];
-                  response.data.data.forEach(element => {
-                    const temp_array=[];
-                    array_dates.push(getMonthName(element.date));
-                    element.events.forEach(subElement=>{
-                      temp_array.push(formatter.format(Date.parse(subElement.arrival_time)));
-                      if(subElement.departure_time!=null)
-                        temp_array.push(formatter.format(Date.parse(subElement.departure_time)));
-                      else{
-                        temp_array.push(null);
-                      }  
-                      temp_array.push(subElement.difference);
-                      temp_array.push(subElement.user.username);
-                        if(subElement.user.profile_url!==null){
-                          let profile_url = subElement.user.profile_url.substring(subElement.user.profile_url.indexOf("\\let-me-in\\"));
-                          profile_url = myIPv4+profile_url;
-                          profile_url = profile_url.replace(/\\/g,"/");
-                          temp_array.push(profile_url);
-                      }else{
-                        temp_array.push(subElement.user.profile_url);
-                      }
-      
-                      temp_array.push(subElement.user.user_detail.car_type);
-                      temp_array.push(subElement.user.user_detail.car_plate_number);
-                      
-                    });
-                    sub_array_event_details.push(temp_array);
+
+        if (!called){
+            axios.get(`${base_url}get_events`,header)
+            .then(function (response) {
+              if(response.data.data.length !==0){
+                const formatter = new Intl.DateTimeFormat("en-GB", {
+                  hour:'2-digit',
+                  minute:'2-digit',
+                  second:'2-digit',
+                  hour12: true
+                });
+                const array_dates = [];
+                const sub_array_event_details=[];
+                response.data.data.forEach(element => {
+                  const temp_array=[];
+                  array_dates.push(getMonthName(element.date));
+                  element.events.forEach(subElement=>{
+                    temp_array.push(formatter.format(Date.parse(subElement.arrival_time)));
+
+                    if(subElement.departure_time!=null)
+                      temp_array.push(formatter.format(Date.parse(subElement.departure_time)));
+                    else{
+                      temp_array.push(null);
+                    }  
                     
-                   
-                  });
-    
-                  const array1=[];
-                  let array2 = [];
-                  let array3=[];
+                    temp_array.push(subElement.difference);
+                    temp_array.push(subElement.user.username);
 
-                  for(let i=0;i<array_dates.length;i++){
-                    let counter =0;
-                    while(counter<sub_array_event_details[i].length){
-                      array3.push(sub_array_event_details[i][counter]);
-
-                      if((counter+1)%7===0){
-                        array2.push(array3);
-                        array3=[];
-                      }
-    
-                      counter++;
+                      if(subElement.user.profile_url!==null){
+                        let profile_url = subElement.user.profile_url.substring(subElement.user.profile_url.indexOf("\\let-me-in\\"));
+                        profile_url = myIPv4+profile_url;
+                        profile_url = profile_url.replace(/\\/g,"/");
+                        temp_array.push(profile_url);
+                    }else{
+                      temp_array.push(subElement.user.profile_url);
                     }
-                    array1.push(array2);
-                    array2=[];
-                    array3=[]; 
+    
+                    temp_array.push(subElement.user.user_detail.car_type);
+                    temp_array.push(subElement.user.user_detail.car_plate_number);
+                    
+                  });
+                  sub_array_event_details.push(temp_array);
+                  
+                  
+                });
+  
+                const array1=[];
+                let array2 = [];
+                let array3=[];
+
+                for(let i=0;i<array_dates.length;i++){
+                  let counter =0;
+                  while(counter<sub_array_event_details[i].length){
+                    array3.push(sub_array_event_details[i][counter]);
+
+                    if((counter+1)%7===0){
+                      array2.push(array3);
+                      array3=[];
+                    }
+                    counter++;
                   }
-      
-                  setDates(array_dates);
-                  setEvents(array1);
+                  array1.push(array2);
+                  array2=[];
+                  array3=[]; 
                 }
-            })
-            .catch(function (error) {
-            });
-          
-            setCalled(true);
-          }
+    
+                setDates(array_dates);
+                setEvents(array1);
+              }
+          })
+          .catch(function (error) {
+          });
+        
+          setCalled(true);
+        }
          
           }
           useEffect( () => {getEvents();});
 
           const togglelayoutVisibility =()=>{
+
             if(isDrawerVisible){
                 setIsDrawerVisible(false);
             }else{
                 setIsDrawerVisible(true);
             }
+
           }
 
           if(dates.length!==0){
+
             return(
               <>
                 <div className='land'>
-                 <Layout pageName='events'  isDrawerVisible={isDrawerVisible} setIsDrawerVisible={setIsDrawerVisible}/>
-                 <div className='land-content'>
-                  <LayoutToggle onClick={togglelayoutVisibility} className='position-static'/>
-                  {dates.map((element,index)=>{
-                     return (
-                      <div className='event-contents' key={index}>
-                        <PageTitle text={element}/>
-                        {events[index].map((subElement,subIndex)=>{
-                          return(
-                            <>
-                            <div>
-                              <div className='box-container full event-time-info' key={subIndex}>
-                                <BoxEvent username={events[index][subIndex][3]} source={events[index][subIndex][4]} car_type={events[index][subIndex][5]} car_plate_number={events[index][subIndex][6]} arrival_time={events[index][subIndex][0]} departure_time={events[index][subIndex][1]} difference_time={events[index][subIndex][2]}/>
+                  <Layout pageName='events'  isDrawerVisible={isDrawerVisible} setIsDrawerVisible={setIsDrawerVisible}/>
+                  <div className='land-content'>
+                    <LayoutToggle onClick={togglelayoutVisibility} className='position-static'/>
+                    {dates.map((element,index)=>{
+                      return (
+                        <div className='event-contents' key={index}>
+                          <PageTitle text={element}/>
+                          {events[index].map((subElement,subIndex)=>{
+                            return(
+                              <>
+                              <div>
+                                <div className='box-container full event-time-info' key={subIndex}>
+                                  <BoxEvent username={events[index][subIndex][3]} source={events[index][subIndex][4]} car_type={events[index][subIndex][5]} car_plate_number={events[index][subIndex][6]} arrival_time={events[index][subIndex][0]} departure_time={events[index][subIndex][1]} difference_time={events[index][subIndex][2]}/>
+                                </div>
                               </div>
-                            </div>
-                            </>
-                          );
-                          })}
-                     </div>
-                      );
-                  })}
-                 </div>
+                              </>
+                            );
+                            })}
+                      </div>
+                        );
+                    })}
+                  </div>
                 </div>
               </>
             );
           }
-          
           else{
             return(
               <>
