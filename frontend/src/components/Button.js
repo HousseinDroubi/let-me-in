cd2c714594a4337b8a866478a9f63477aaaccb19
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-25 22:30:14 +0200

    Frontend-button component: clean code

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -3,7 +3,9 @@ import classnames from 'classnames';
 
 const Button = (props)=>{
     
-    return <button className={classnames( "button",props.className)} onClick={props.onClick}>{props.name}</button>
+    return (
+        <button className={classnames( "button",props.className)} onClick={props.onClick}>{props.name}</button>
+    );    
     
 }
 export default Button;
