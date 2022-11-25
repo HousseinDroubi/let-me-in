796d22b9dff941c6bbf35b830909c3b5edf22881
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-25 21:15:52 +0200

    Frontend-label component: clean code

--- frontend/src/components/Label.js
+++ frontend/src/components/Label.js
@@ -1,6 +1,11 @@
 import '../App.css';
 import classnames from 'classnames';
+
 const Label = (props)=>{
-    return <p  className={classnames( "label",props.className)}>{props.title}</p>
+
+    return  ( 
+        <p className={classnames( "label",props.className)}>{props.title}</p>
+    );    
+    
 }
 export default Label;
