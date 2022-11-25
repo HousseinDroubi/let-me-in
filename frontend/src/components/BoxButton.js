0683c1d7878919ca4d6bab31c48f4bd3297cae21
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-25 22:29:11 +0200

    Frontend-box button component: clean code

--- frontend/src/components/BoxButton.js
+++ frontend/src/components/BoxButton.js
@@ -3,10 +3,12 @@ import classnames from 'classnames';
 
 const BoxButton = (props)=>{
 
-    return <button className={classnames( "box-button",props.color==='red'?'box-button-background-red':'box-button-background-blue',props.className)} onClick={props.onClick}>
+    return (
+            <button className={classnames( "box-button",props.color==='red'?'box-button-background-red':'box-button-background-blue',props.className)} onClick={props.onClick}>
                 <img src = {props.icon} alt=''/>
                 <p>{props.text}</p>
             </button>
+    );        
     
 }
 export default BoxButton;
