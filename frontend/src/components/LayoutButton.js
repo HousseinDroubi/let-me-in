3df6b01d0c10d9a397128e666ea247f8139f0b94
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-11-25 21:20:07 +0200

    Frontend-layout button component: clean code

--- frontend/src/components/LayoutButton.js
+++ frontend/src/components/LayoutButton.js
@@ -1,13 +1,13 @@
 import '../App.css';
 import classnames from 'classnames';
+
 const DrawerButton = (props)=>{
 
-    return (<button className={classnames( "button-drawer",props.className)} onClick={props.onClick}>
-        <p>{props.name}</p>
-        <div 
-        className={classnames( "circle",props.isWaiting?'display-block':'display-none',props.className)}
-        ></div>
+    return (
+        <button className={classnames( "button-drawer",props.className)} onClick={props.onClick}>
+            <p>{props.name}</p>
         </button>
     );
+
 }
 export default DrawerButton;
